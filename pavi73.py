935c053c7977fdc26588f63e27e7a30835628b2f
paviprajansai <52271026+paviprajansai@users.noreply.github.com>
2019-07-19 16:22:08 +0530

    Create pavi73.py

---
+++ pavi73.py
@@ -0,0 +1,17 @@
+// function to find factorial of given number 
+unsigned int factorial(unsigned int n) 
+{ 
+    if (n == 0) 
+    return 1; 
+    return n * factorial(n - 1); 
+} 
+  
+// Driver code 
+int main() 
+{ 
+    int num = 5; 
+    cout << "Factorial of " << num << " is " << factorial(num) << endl; 
+    return 0; 
+} 
+  
+// This code is contributed by Shivi_Aggarwal 
